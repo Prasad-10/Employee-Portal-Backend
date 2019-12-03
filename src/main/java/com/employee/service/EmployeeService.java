790553fbbefc46c671bc77f6b82c397532004c76
Renuka Prasad G <47290039+Prasad-10@users.noreply.github.com>
2019-12-03 15:16:49 +0530

    Update EmployeeService.java

--- src/main/java/com/employee/service/EmployeeService.java
+++ src/main/java/com/employee/service/EmployeeService.java
@@ -7,7 +7,7 @@ import com.employee.model.Employee;
 
 public interface EmployeeService {
 
-	public Employee saveEmployee(Employee employee) throws ParseException;
+	public Employee saveEmployee(Employee employee);
 	
 	public List<Employee> getAllEmployees();
 }
